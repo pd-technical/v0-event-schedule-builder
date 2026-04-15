5bcda54e9c6af6c173127d9286d0328e8f6bd242
srutie <sruti.elan@gmail.com>
2026-04-14 20:30:43 -0700

    Update run sync to get database

--- scripts/run-sync.ts
+++ scripts/run-sync.ts
@@ -1,18 +1,27 @@
+import sync from "@/app/lib/db/syncEvents";
 import "dotenv/config";
 
 async function runSync() {
   // make a POST request to the sync endpoint with the secret in the authorization header
-  const res = await fetch("http://localhost:3000/api/admin/sync-events", {
-    method: "POST",
-    headers: {
-      "authorization": `Bearer ${process.env.SYNC_SECRET}`,
-    },
-  });
+  // const res = await fetch("http://localhost:3000/api/admin/sync-events", {
+  //   method: "POST",
+  //   headers: {
+  //     "authorization": `Bearer ${process.env.SYNC_SECRET}`,
+  //   },
+  // });
 
-  const text = await res.text();
+  // const text = await res.text();
 
-  console.log("Status:", res.status);
-  console.log("Response:", text);
+  // console.log("Status:", res.status);
+  // console.log("Response:", text);
+
+  try {
+    await sync();
+    console.log("Events synchronized successfully");
+  } catch (error) {
+    console.error("Error synchronizing events:", error);
+    process.exit(1);
+  }
 }
 
 runSync();
